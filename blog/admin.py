8272cd451a72f9b3026eff1c34645752424d917d
Anthony <tonyudeagbala@gmail.com>
2023-02-23 11:00:57 +0100

    chore: register models

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Article, Comment
+
+
+class CommentInline(admin.TabularInline):
+    model = Comment
+
+
+class ArticleAdmin(admin.ModelAdmin):
+    list_display = ('title',)
+    inlines = [CommentInline]
+
+
+admin.site.register(Article, ArticleAdmin)
